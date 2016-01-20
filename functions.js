5106619e1c9c84c204a55c0343dc0c23ebbe12eb
David Doan <Doan@dhcp-18-111-121-54.dyn.mit.edu>
2016-01-20 18:13:24 -0500

    Added userInfo, balanceInfo, and oauthRefresh functions

--- functions.js
+++ functions.js
@@ -1,31 +1,83 @@
 var express = require('express');
+var https = require('https');
 var request = require('request');
 var url = require('url');
+var coinbase = require('coinbase').Client;
 
 var clientID = "fa908ac547fc45176e3e735851cd6c504fd853cf1f0d91e2a5192d9fa6f3e89a";
 var clientSecret = "03dcc5dde881365e012ae61da754b63b41371d2bf5b0c9712b92d93c522c1b2b";
 
 module.exports = {
 
-	oauthToken : function (code){
+	oauthToken : function (code) {
 
-	var optionsAuth = {
-		url : 'https://api.coinbase.com/oauth/token',
-		method : 'POST',
-		form : {	
-			'grant_type' : 'authorization_code',
-			'code': code,
-			'client_id' : clientID,
-			'client_secret' : clientSecret,
-			'redirect_uri' : 'https://www.google.com/',
-		},
-	};
+		var optionsAuth = {
+			url : 'https://api.coinbase.com/oauth/token',
+			method : 'POST',
+			form : {	
+				'grant_type' : 'authorization_code',
+				'code': code,
+				'client_id' : clientID,
+				'client_secret' : clientSecret,
+				'redirect_uri' : 'https://www.google.com/',
+			},
+		};
 
-	request.post(optionsAuth, function (error,response,body) {
-        console.log('what');
-        console.log(body);
-        console.log(error);
-	});
+		request.post(optionsAuth, function (error, token) {
+		    console.log(token);
+		});
+	},
 
-	}
-}
+	oauthRefresh : function (refreshToken) {
+ 	
+ 		var optionsAuth = {
+ 			url : 'https://api.coinbase.com/oauth/token',
+ 			method: 'POST',
+ 			form : {
+ 				'grant_type' : 'refresh_token',
+ 				'redirect_uri' : 'https://www.google.com/',
+ 				'client_id' : clientID,
+ 				'client_secret' : clientSecret,
+ 				'refreshToken' : refreshToken,
+ 			},
+
+ 		};
+
+ 		request.post(optionsAuth, function (error, token) {
+ 			console.log(token);
+ 		});
+	},
+
+	userInfo : function (accessToken) {
+
+		var client = new coinbase({
+			'accessToken' : accessToken, 
+		});
+
+		client.getCurrentUser(function (error, user) {
+				console.log(user);
+		});
+
+	},
+
+	balanceInfo : function (accessToken) {
+
+		var client = new coinbase({
+			'accessToken' : accessToken, 
+		});
+
+		client.getAccounts({}, function (error, accounts, res) {
+			if (res.status == 401) {
+				res.send('getfucked')
+				oauthRefresh(refreshToken);
+			}
+
+			accounts.forEach(function(acct) {
+    			console.log('my bal: ' + acct.balance.amount + ' for ' + acct.name);
+  			});
+		});
+
+	},
+
+
+};
--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/oauth', function(req, res) {
+router.get('/oauthToken', function(req, res) {
   functions.oauthToken(req.query.code);
+});
+
+router.get('/userInfo', function(req, res) {
+  functions.userInfo(req.query.accessToken);
+});
+
+router.get('/balanceInfo', function(req, res) {
+  functions.balanceInfo(req.query.accessToken);
 });
 
 module.exports = router;
